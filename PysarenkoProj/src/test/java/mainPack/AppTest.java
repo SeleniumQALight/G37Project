10d7f498e820b2f0e2d556fcc46739314d20b314
bpysarenko <4spysar@gmail.com>
2018-10-30 21:13:06 +0200

    First commit without blood!

--- PysarenkoProj/src/test/java/mainPack/AppTest.java
+++ PysarenkoProj/src/test/java/mainPack/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App. Supper double check update.
  */
 public class AppTest 
 {
