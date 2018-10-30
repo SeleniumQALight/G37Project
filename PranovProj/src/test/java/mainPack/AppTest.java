bf548b414ba7cf0bc2acd5470c27827823d008fd
vladislav.pranov <vladislav.pranov@metricinsights.com>
2018-10-30 21:16:04 +0300

    second commit add change to the 1st comment

--- PranovProj/src/test/java/mainPack/AppTest.java
+++ PranovProj/src/test/java/mainPack/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App...234..
+ * Unit test for simple App...234..543..
  */
 public class AppTest 
 {
