eddd773d5a113ea307fd25a27e754b779a42086d
OlenaG <olena.h.w@gmail.com>
2018-10-30 21:15:44 +0200

    Second commit

--- GavrilukProj/src/test/java/mainPack/AppTest.java
+++ GavrilukProj/src/test/java/mainPack/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App. Something
+ * Unit test for simple App. Something. Something else.
  */
 public class AppTest 
 {
