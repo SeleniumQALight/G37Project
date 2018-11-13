815b53519de02fa625b7c4c34fc1a7eb1c539b94
tkachenko <summy3333@gmail.com>
2018-11-13 21:39:55 +0200

    page object

---
+++ TkachenkoProj/src/main/java/pages/LoginPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.ExecutionException;
+
+public class LoginPage extends ParentPage{
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+    public void openLoginPage(){
+        try {
+            driver.get("http://v3.test.itpmgroup.com");
+            logger.info("Login page was opened");
+        }catch (Exception e){
+            logger.error("Can not open Login Page");
+            Assert.fail("Can not open Login Page");
+        }
+    }
+
+    public void enterLogin(String login) {
+        try{
+            driver.findElement(By.name("_username")).clear();
+            driver.findElement(By.name("_username")).sendKeys(login);
+            logger.info(login + " was inputted into Input");
+        }catch (Exception e){
+            logger.error("Can not work with element");
+            Assert.fail("Can not work with element");
+        }
+    }
+    public void enterPassword(String passWord) {
+        try{
+            driver.findElement(By.name("_password")).clear();
+            driver.findElement(By.name("_password")).sendKeys(passWord);
+            logger.info(passWord + " was inputted into Input");
+        }catch (Exception e){
+            logger.error("Can not work with element");
+            Assert.fail("Can not work with element");
+        }
+    }
+}
