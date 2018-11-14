408528d83075164cd4ea265ea1dc8fad0e30da7a
dimakotelnikov <kotelnikovdmitriyv@gmail.com>
2018-11-14 10:47:23 +0200

    commi

--- KotelnikovProj/src/test/java/logIn/LogInNew.java
+++ KotelnikovProj/src/test/java/logIn/LogInNew.java
@@ -1,17 +1,1 @@
-package logIn;
-
-import org.junit.Assert;
-import org.junit.Test;
-import parentTest.ParentTest;
-
-public class LogInNew extends ParentTest {
-    @Test
-    public void validLogin(){
-        loginPage.openLoginPage();
-        loginPage.enterLogin("Student");
-        loginPage.enterPassword("909090");
-        loginPage.clickButtonVhod();
-
-        Assert.assertTrue("Avatar is not present", homePage.isAvatarPresent());
-    }
-}
+package logIn;import org.junit.Assert;import org.junit.Test;import parentTest.ParentTest;public class LogInNew extends ParentTest {    @Test    public void validLogin(){        loginPage.openLoginPage();        loginPage.enterLogin("Student");        loginPage.enterPassword("909090");        loginPage.clickButtonVhod();        Assert.assertTrue("Avatar is not present", homePage.isAvatarPresent());    }}
