5be941a67d17b262198a99ca34265c06d83195c2
tkachenko <summy3333@gmail.com>
2018-11-15 21:18:47 +0200

    15-11-18 pageObject

--- TkachenkoProj/src/main/java/pages/LoginPage.java
+++ TkachenkoProj/src/main/java/pages/LoginPage.java
@@ -3,14 +3,26 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import javax.naming.Name;
 import java.util.concurrent.ExecutionException;
 
 public class LoginPage extends ParentPage{
+    @FindBy(name = "_username")
+    WebElement inputLogin;
+
+    @FindBy(name = "_password")
+    WebElement inputPassword;
+
+    @FindBy(tagName = "button")
+    WebElement buttonVhod;
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
+
     public void openLoginPage(){
         try {
             driver.get("http://v3.test.itpmgroup.com");
@@ -22,33 +34,20 @@ public class LoginPage extends ParentPage{
     }
 
     public void enterLogin(String login) {
-        try{
-            driver.findElement(By.name("_username")).clear();
-            driver.findElement(By.name("_username")).sendKeys(login);
-            logger.info(login + " was inputted into Input");
-        }catch (Exception e){
-            logger.error("Can not work with element");
-            Assert.fail("Can not work with element");
-        }
+        actionsWithOurElements.enterTextIntoElement(inputLogin, login);
     }
     public void enterPassword(String passWord) {
-        try{
-            driver.findElement(By.name("_password")).clear();
-            driver.findElement(By.name("_password")).sendKeys(passWord);
-            logger.info(passWord + " was inputted into Input");
-        }catch (Exception e){
-            logger.error("Can not work with element");
-            Assert.fail("Can not work with element");
-        }
+        actionsWithOurElements.enterTextIntoElement(inputPassword, passWord);
     }
 
     public void clickButtonVhod() {
-        try {
-            driver.findElement(By.xpath(".//button[@type='submit']")).click();
-            logger.info("User is successfully logged in");
-        }catch (Exception e){
-            logger.error("Can not work with element");
-            Assert.fail("Can not work with element");
-        }
+        actionsWithOurElements.clickOnElement(buttonVhod);
+//        try {
+//            buttonVhod.click();
+//            logger.info("User is successfully logged in");
+//        }catch (Exception e){
+//            logger.error("Can not work with element");
+//            Assert.fail("Can not work with element");
+//        }
     }
 }
--- TkachenkoProj/src/main/java/pages/ParentPage.java
+++ TkachenkoProj/src/main/java/pages/ParentPage.java
@@ -1,13 +1,18 @@
 package pages;
 
+import libs.ActionsWithOurElements;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 abstract public class ParentPage {
     WebDriver driver;
     Logger logger = Logger.getLogger(getClass());
+    ActionsWithOurElements actionsWithOurElements;
 
     public ParentPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver,this);
+        actionsWithOurElements = new ActionsWithOurElements(driver);
     }
 }
