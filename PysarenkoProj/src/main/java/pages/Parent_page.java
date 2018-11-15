c0749e7213a75d52a43aed57cbac294829208237
bpysarenko <4spysar@gmail.com>
2018-11-15 21:18:12 +0200

    ClassWork commit

--- PysarenkoProj/src/main/java/pages/Parent_page.java
+++ PysarenkoProj/src/main/java/pages/Parent_page.java
@@ -1,13 +1,19 @@
 package pages;
 
+import libs.ActionsWithOurElements;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 abstract public class Parent_page {
     WebDriver webDriver;
+
     Logger logger = Logger.getLogger(getClass());
+    ActionsWithOurElements actionsWithOurElements;
 
     public Parent_page(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver,this);
+        actionsWithOurElements = new ActionsWithOurElements(webDriver);
     }
 }
