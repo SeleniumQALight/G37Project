f388f9fb765e4ac9b08bfeffbe682f3895601af9
Teplova <teplovayanina@gmail.com>
2018-11-15 21:18:20 +0200

    Made test easier

--- TeplovaProj/src/main/java/pages/LoginPage.java
+++ TeplovaProj/src/main/java/pages/LoginPage.java
@@ -1,13 +1,25 @@
 package pages;
 
+import libs.ActionWithOurElements;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by 123 on 13.11.2018.
  */
 public class LoginPage extends ParentPage {
+
+    @FindBy (name = "_username")
+    WebElement inputLogin;
+
+    @FindBy (name = "_password")
+    WebElement inputPassword;
+
+    @FindBy (xpath = ".//button")
+    WebElement buttonEnter;
 
 
     public LoginPage(WebDriver webDriver) {
@@ -16,7 +28,7 @@ public class LoginPage extends ParentPage {
 
     public void openLoginPage() {
         try {
-            webDriver.get("http://v3.test.itpmgroup.com/");
+            webDriver.get("http://v3.test.itpmgroup.com");
             logger.info(" Web page is opened");
 
 
@@ -28,59 +40,64 @@ public class LoginPage extends ParentPage {
     }
 
     public void enterLogin(String login) {
-        try
-
-        {
-            webDriver.findElement(By.name("_username")).clear();
-            webDriver.findElement(By.name("_username")).sendKeys();
-            logger.info(login);
-
-        } catch (
-                Exception e )
-
-        {
-            logger.error("Can't enter login");
-            Assert.fail("Can't enter login");
-        }
+        actionWithOurElements.enterTextIntoElement(inputLogin, login);
+//        try
+//
+//        {
+//            inputLogin.clear();
+//            inputLogin.sendKeys();
+//            logger.info(login);
+//
+//        } catch (
+//                Exception e )
+//
+//        {
+//            logger.error("Can't enter login");
+//            Assert.fail("Can't enter login");
+//        }
     }
 
 
     public void enterPassword(String password) {
+        actionWithOurElements.enterTextIntoElement(inputPassword, password);
 
-        try
-
-        {
-            webDriver.findElement(By.name("_username")).clear();
-            webDriver.findElement(By.name("_password")).sendKeys("909090");
-
-            logger.info(password + "password was added");
-
-        } catch (
-                Exception e )
-
-        {
-            logger.error("Can't enter login");
-            Assert.fail("Can't enter login");
-
-        }
+//        try
+//
+//        {
+//            inputPassword.clear();
+//            inputPassword.sendKeys("909090");
+//
+//            logger.info(password + "password was added");
+//
+//        } catch (
+//                Exception e )
+//
+//        {
+//            logger.error("Can't enter login");
+//            Assert.fail("Can't enter login");
+//
+//        }
     }
 
 
-    public void clickButtonEnter(String buttton) {
+    public void clickButtonEnter() {
 
-        try
+        actionWithOurElements.clickOnElement(buttonEnter);
 
-        {
-            webDriver.findElement(By.xpath(".//button")).click();
-            logger.info(buttton + "button is found");
 
-        } catch (
-                Exception e )
-
-        {
-            logger.error("Password or Username are incorrect");
-            Assert.assertTrue( "Password or Username are incorrect", true );
-
-        }
+//        try
+//
+//        {
+//            buttonEnter.click();
+//            logger.info("button is found");
+//
+//        } catch (
+//                Exception e )
+//
+//        {
+//            logger.error("Password or Username are incorrect");
+//            Assert.assertTrue( "Password or Username are incorrect", true );
+//
+//        }
     }
 }
