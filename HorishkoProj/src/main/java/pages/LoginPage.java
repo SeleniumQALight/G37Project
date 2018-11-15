6746452ac4d5912212815c7dd92021052604e923
Margo <margarita.khorishko@gmail.com>
2018-11-15 21:21:29 +0200

    actionsWithOurElements added

--- HorishkoProj/src/main/java/pages/LoginPage.java
+++ HorishkoProj/src/main/java/pages/LoginPage.java
@@ -3,8 +3,19 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends ParentPage {
+    @FindBy(name = "_username")
+    WebElement inputLogin;
+
+    @FindBy(name = "_password")
+    WebElement inputPassWord;
+
+    @FindBy(tagName = "button")
+    WebElement buttonVhod;
+
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -21,34 +32,41 @@ public class LoginPage extends ParentPage {
     }
 
     public void enterLogin(String login) {
-        try {
-            webDriver.findElement(By.name("_username")).clear();
-            webDriver.findElement(By.name("_username")).sendKeys(login);
-            logger.info(login + "was inputted into Input");
-        } catch (Exception e) {
-            logger.error("Cannot work with element");
-            Assert.fail("Cannot work with element");
-        }
+        actionsWithOurElements.enterTextInToElement(inputLogin,login);
+
+//        try {
+//            inputLogin.clear();
+//            inputLogin.sendKeys(login);
+//            logger.info(login + "was inputted into Input");
+//        } catch (Exception e) {
+//            logger.error("Cannot work with element");
+//            Assert.fail("Cannot work with element");
+//        }
     }
-
         public void enterPassword(String passWord) {
-            try {
-                webDriver.findElement(By.name("_password")).clear();
-                webDriver.findElement(By.name("_password")).sendKeys(passWord);
-                logger.info(passWord + "was inputted into Input");
+            actionsWithOurElements.enterTextInToElement(inputPassWord,passWord); // ниже код, который делает то же самое
 
-            }catch (Exception e){
-                logger.error("Cannot work with element");
-                Assert.fail("Cannot work with element");
-            }
+//            try {
+//                inputPassWord.clear();
+//                inputPassWord.sendKeys(passWord);
+////                webDriver.findElement(By.name("_password")).clear();
+////                webDriver.findElement(By.name("_password")).sendKeys(passWord);
+//                logger.info(passWord + "was inputted into Input");
+//            }catch (Exception e){
+//                logger.error("Cannot work with element");
+//                Assert.fail("Cannot work with element");
+//            }
     }
     public void clickButtonVhod() {
-        try {
-            webDriver.findElement(By.tagName("button")).click();
-            logger.info("click on button vhod");
-        }catch (Exception e){
-            logger.error("Cannot work with element");
-            Assert.fail("Cannot work with element");
-        }
+        actionsWithOurElements.clickOnElement(buttonVhod);
+
+//        try {
+//            buttonVhod.click();
+// //           webDriver.findElement(By.tagName("button")).click();
+//            logger.info("click on button vhod");
+//        }catch (Exception e){
+//            logger.error("Cannot work with element");
+//            Assert.fail("Cannot work with element");
+//        }
     }
 }
