a9ade08a53b2da1d5669fa7ff6b3aafa855fea4f
bpysarenko <4spysar@gmail.com>
2018-11-17 12:54:21 +0200

    ClassWork commit

--- PysarenkoProj/src/main/java/pages/HomePage.java
+++ PysarenkoProj/src/main/java/pages/HomePage.java
@@ -3,19 +3,21 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HomePage extends Parent_page{
+public class HomePage extends Parent_page {
 
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
-    public boolean isAvatarPresent(){
+    public boolean isAvatarPresent() {
         try {
             return webDriver.findElement(By.xpath(".//*[@class='pull-left image']"))
                     .isDisplayed();
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
+
+
 }
