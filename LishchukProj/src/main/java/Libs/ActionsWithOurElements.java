47e9314f9c879187fe7086189498ff53dde4a851
oleh <olishchuk@themeetgroup.com>
2018-11-20 15:04:26 +0200

    actionsWithOurElements was added

--- LishchukProj/src/main/java/Libs/ActionsWithOurElements.java
+++ LishchukProj/src/main/java/Libs/ActionsWithOurElements.java
@@ -1,5 +1,7 @@
-package Libs;
+package libs;
 
+import org.apache.commons.logging.impl.Jdk14Logger;
+import org.apache.commons.logging.impl.SimpleLog;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -9,30 +11,32 @@ import javax.xml.soap.Text;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
-    Logger logger = Logger.getLogger(getClass());
 
     public ActionsWithOurElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        Logger logger = Logger.getLogger(getClass());
     }
 
-    public void enterTextIntoElement (WebElement element, String text)
-     try {
-        element.clear();
-        element.sendKeys(Text);
-        logger.info(Text + "was inputted into input");
-    } catch (Exception e) {
-        logger.error("Cannot work with element" + e);
-        Assert.fail("Cannot work with element" + e);
+    public void enterTextInToElement(WebElement element, String text) {
+        SimpleLog logger = null;
+        try {
+            element.clear();
+            element.sendKeys(text);
+            logger.info(text + "was inputted into input");
+        }catch (Exception e) {
+            logger.error("Cannot work with element" + e);
+            Assert.fail("Cannot work with element" + e);
+        }
     }
 
-    public void clickOnElement (WebElement element) {
-         try {
-             element.click();
-             Logger.info("Element was clicked" + e);
-         } catch (Exception e) {
-             logger.error("Cannot work with element" + e);
-             Assert.fail("Cannot work with element" + e);
-
-         }
+    public void clickOnElement(WebElement element) {
+        SimpleLog logger = null;
+        try {
+            element.click();
+            logger.info("Element was clicked");
+        }catch (Exception e) {
+            logger.error("Cannot work with element" + e);
+            Assert.fail("Cannot work with element" + e);
+        }
     }
 }
