5af968794c3e7104c5f6f006a9d1c39aa612f309
oleh <olishchuk@themeetgroup.com>
2018-11-20 11:52:42 +0200

    Test 2 - click button Vhod

--- LishchukProj/src/main/java/pages/LoginPage.java
+++ LishchukProj/src/main/java/pages/LoginPage.java
@@ -41,4 +41,13 @@ public class LoginPage extends ParentPage {
             Assert.fail("Cannot work with element");
         }
     }
+    public void clickButtonVhod() {
+        try {
+            webDriver.findElement(By.tagName("button")).click();
+            logger.info("was clicked");
+        }catch (Exception e) {
+            logger.error("Cannot find such a button");
+            Assert.fail("Cannot find such a button");
+        }
+    }
 }
