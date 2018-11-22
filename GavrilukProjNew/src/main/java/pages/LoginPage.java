f09904e948307446e38a41faed96c874a6f75564
radulenko <radulenko@gmail.com>
2018-11-22 16:43:09 +0200

    new module was added

---
+++ GavrilukProjNew/src/main/java/pages/LoginPage.java
@@ -0,0 +1,62 @@
+package pages;
+
+import org.junit.Assert;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends ParentPage {
+    @FindBy(name = "_username")
+    WebElement inputLogin;
+
+    @FindBy(name = "_password")
+    WebElement inputPassWord;
+
+    @FindBy(tagName = "button")
+    WebElement buttonVhod;
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver,"/login");
+    }
+
+    public void openLoginPage(){
+        try{
+            webDriver.get("http://v3.test.itpmgroup.com");
+            logger.info("Login page was opened");
+        }catch (Exception e){
+            logger.error("Can not open Login Page");
+            Assert.fail("Can not open Login Page");
+        }
+    }
+
+    public void enterLogin(String login) {
+      actionsWithOurElements.enterTextInToElement(inputLogin,login);
+    }
+
+    public void enterPassword(String passWord) {
+        actionsWithOurElements.enterTextInToElement(inputPassWord,passWord);
+    }
+
+    public void clickButtonVhod() {
+        actionsWithOurElements.clickOnElement(buttonVhod);
+    }
+
+    public boolean isButtonVhodDisplayed(){
+        return actionsWithOurElements.isElementDisplayed(buttonVhod);
+    }
+
+    public void loginInToApp(String login,String passWord){
+        openLoginPage();
+        enterLogin(login);
+        enterPassword(passWord);
+        clickButtonVhod();
+    }
+
+    public void validLoginInToApp(){
+        loginInToApp("Student", "909090");
+        HomePage homePage = new HomePage(webDriver);
+        homePage.isAvatarDislpayed();
+
+    }
+}
