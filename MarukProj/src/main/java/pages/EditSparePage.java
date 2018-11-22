faf64f86f85c7f3668f9feac2fbe8212bd86192f
KateMaruk <marukkatia@gmail.com>
2018-11-22 21:43:00 +0200

    Added new method "Create New Spare" 22.11.2018

---
+++ MarukProj/src/main/java/pages/EditSparePage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditSparePage extends ParentPage {
+    @FindBy(id = "spares_spareName")
+    private WebElement spareNameInput;
+
+    @FindBy(id  = "spares_spareType")
+    private WebElement spareTypeDD;
+
+    @FindBy(name = "delete")
+    private  WebElement buttonDelete;
+
+    @FindBy(name = "add")
+    private WebElement buttonCreate;
+
+    public EditSparePage(WebDriver webDriver) {
+        super(webDriver, "/dictionary/spares/edit");
+    }
+
+    public boolean checkSpareNameInInput(String sparename){
+        return spareNameInput.getText().equals(sparename);
+    }
+
+    public void clickOnButtonDelete(){
+        actionsWithOurElements.clickOnElement(buttonDelete);
+
+    }
+
+    public void enterSpareNameIntoInput(String spareName) {
+        actionsWithOurElements.enterTextToElement(spareNameInput, spareName);
+    }
+
+    public void clickOnButtonCreate() {
+        actionsWithOurElements.clickOnElement(buttonCreate);
+    }
+
+    public void selectSpareTypeInDD(String spareType) {
+        actionsWithOurElements.selectTextInDD(spareTypeDD, spareType);
+    }
+}
