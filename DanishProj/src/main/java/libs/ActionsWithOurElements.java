1d678cc2b83306f8a87c3a6e5b95940316424437
maksdanish <44396943+maksdanish@users.noreply.github.com>
2018-11-22 21:41:18 +0200

    Danish-22-11

--- DanishProj/src/main/java/libs/ActionsWithOurElements.java
+++ DanishProj/src/main/java/libs/ActionsWithOurElements.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
@@ -48,6 +49,17 @@ public class ActionsWithOurElements {
 
         }catch (Exception e){
             return false;
+        }
+    }
+
+    public void selectTextInDD(WebElement element, String text) {
+        try {
+            Select select = new Select(element);
+            select.selectByVisibleText(text);
+            logger.info(text + "was selected in DD");
+        } catch (Exception e) {
+            logger.error("Cannot work with element" + e);
+            Assert.fail("Cannot work with element" + e);
         }
     }
 }
--- DanishProj/src/test/java/spares/AddNewSpare.java
+++ DanishProj/src/test/java/spares/AddNewSpare.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import parentTest.ParentTest;
 
 public class AddNewSpare extends ParentTest {
+    String spareName = "danishSpare";
     @Test
     public void addNewSpare(){
         loginPage.validLoginInToApp();
@@ -11,7 +12,11 @@ public class AddNewSpare extends ParentTest {
         homePage.clickOnMenuDictionaty();
         homePage.clickOnSubMenuSpare();
         sparePage.checkUrl();
-
+        sparePage.checkAndDeleteSpare(spareName);
+        sparePage.clickOnAddButton();
+        editSparePage.enterSpareNameInToInput(spareName);
+        editSparePage.selectSpareTypeInDD("Механика1");
+        editSparePage.clickButtonCreate();
 
 
 
