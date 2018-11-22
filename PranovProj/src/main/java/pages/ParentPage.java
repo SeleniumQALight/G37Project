e812c72cc2d9aeecec4ba6d0c2987fe9c102dc3f
vladislav.pranov <vladislav.pranov@metricinsights.com>
2018-11-22 21:42:51 +0300

    detail creating

--- PranovProj/src/main/java/pages/ParentPage.java
+++ PranovProj/src/main/java/pages/ParentPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import libs.ActionWithOurElements;
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -9,11 +10,27 @@ abstract public class ParentPage {
     WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
     ActionWithOurElements actionWithOurElements;
+    String baseUrl = "http://v3.test.itpmgroup.com";
+    String expectedUrl;
 
-    public ParentPage(WebDriver webDriver) {
+    public ParentPage(WebDriver webDriver, String relativeUrl) {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);
         actionWithOurElements = new ActionWithOurElements(webDriver);
+        this.expectedUrl = baseUrl + relativeUrl;
+    }
+
+    public String getCurrentUrl(){
+        return webDriver.getCurrentUrl();
+    }
+
+    public void checkUrl(){
+        try {
+            Assert.assertEquals("Url is not expected", expectedUrl, getCurrentUrl());
+        } catch (Exception e) {
+            logger.error("Can not work with url");
+            Assert.fail("Can not work with url");
+        }
     }
 
 }
