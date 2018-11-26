a2971e0c52c4b02d43ffbafb64d31fdf0f298f48
Liliya Zhukova <polimorfiya@gmail.com>
2018-11-26 18:42:10 +0200

    home work 26.11: 2 Methods: setSpareTypeDDByName  and  setSpareTypeDDByValue

--- ZhukovaProj/src/main/java/pages/EditSparePage.java
+++ ZhukovaProj/src/main/java/pages/EditSparePage.java
@@ -1,11 +1,12 @@
 
- package pages;
+package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class EditSparePage extends ParentPage {
+
 
     @FindBy(id = "spares_spareName")
     private WebElement spareNameInput;
@@ -16,19 +17,27 @@ public class EditSparePage extends ParentPage {
     @FindBy(name = "delete")
     private WebElement buttonDelete;
 
-    @FindBy (name= "add")
+    @FindBy(name = "add")
     private WebElement buttonCreate;
 
+
+    @FindBy(xpath = ".//*[@value='1']")
+    private WebElement value1;
+
+    @FindBy(xpath = ".//*[(text()= 'Механика1')]")
+    private WebElement SpareTypeMehanika1;
 
 
     public EditSparePage(WebDriver webDriver) {
         super(webDriver, "/dictionary/spares/edit");
     }
-    public boolean checkSpareNameInInput(String spareName){
+
+    public boolean checkSpareNameInInput(String spareName) {
         return spareNameInput.getText().equals(spareName);
 
     }
-    public void clickButtonDeleteOnEditPage(){
+
+    public void clickButtonDeleteOnEditPage() {
 
         actionsWithOurElements.clickOnElement(buttonDelete);
     }
@@ -38,7 +47,7 @@ public class EditSparePage extends ParentPage {
         actionsWithOurElements.enterTextIntoElement(spareNameInput, spareName);
     }
 
-    public void selectSpareTypeInDD(String spareType){
+    public void selectSpareTypeInDD(String spareType) {
         actionsWithOurElements.selectTextInDD(spareTypeDD, spareType);
     }
 
@@ -46,5 +55,22 @@ public class EditSparePage extends ParentPage {
     public void clickOnButtonCreate() {
         actionsWithOurElements.clickOnElement(buttonCreate);
     }
+
+
+    public void setSpareTypeDDByName(WebElement element, WebElement elementDD) {
+
+        actionsWithOurElements.clickOnElement(spareTypeDD);
+        actionsWithOurElements.clickOnElement(SpareTypeMehanika1);
+
+    }
+
+    public void setSpareTypeDDByValue(WebElement element, WebElement valueDD) {
+
+        actionsWithOurElements.clickOnElement(spareTypeDD);
+        actionsWithOurElements.clickOnElement(value1);
+
+
+    }
 }
+
 
