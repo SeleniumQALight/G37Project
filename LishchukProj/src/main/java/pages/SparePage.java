c9b112b13d18fe3810f0c1c1937c7da8c6a2bb2d
oleh <olishchuk@themeetgroup.com>
2018-11-27 22:04:05 +0200

    27-11-Lishchuk-SparePart

--- LishchukProj/src/main/java/pages/SparePage.java
+++ LishchukProj/src/main/java/pages/SparePage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.FindBy;
 import java.util.List;
 
 public class SparePage extends ParentPage {
+    EditSparePage editSparePage;
     @FindBy(xpath = "//table//tr")
     List<WebElement> ListOfRows;
 
@@ -17,13 +18,15 @@ public class SparePage extends ParentPage {
 
     public SparePage(WebDriver webDriver) {
         super(webDriver, "/dictionary/spares");
+        editSparePage = new EditSparePage(webDriver);
     }
 
+    @Deprecated
     public void checkAndDeleteSpare(String spareName) {
         EditSparePage editSparePage = new EditSparePage(webDriver);
         if (ListOfRows.size() > 0) {
             for (WebElement line : ListOfRows) {
-                WebElement cellsWithSpare = line.findElement(By.xpath("//td[1]"));
+                WebElement cellsWithSpare = line.findElement(By.xpath("//*[1]"));
                 if (cellsWithSpare.getText().equals(spareName)) {
                     actionsWithOurElements.clickOnElement(cellsWithSpare);
 
@@ -37,5 +40,30 @@ public class SparePage extends ParentPage {
 
     public void clickOnAddButton() {
         actionsWithOurElements.clickOnElement(buttonAdd);
+    }
+
+    public void deletingSpareUntilPresent(String spareName) {
+        int counter = 0;
+        while (isSpareInList(spareName)) {
+            clickOnSpare(spareName);
+            editSparePage.clickButtonCreate();
+            counter++;
+            if (counter > 100) {
+                Assert.fail("There are more than 100 spares in list" +
+                        "or deleting does not work, so the test does not go further");
+            }
+        }
+    }
+
+    public boolean isSpareInList(String spareName) {
+        return actionsWithOurElements.isElementDisplayed(By.xpath(".//*[text() =' " + spareName + "']"));
+    }
+
+    private void clickOnSpare(String spareName) {
+        actionsWithOurElements.clickOnElement(getSpareWithName(spareName));
+    }
+
+    private WebElement getSpareWithName(String spareName) {
+        return webDriver.findElement(By.xpath(".//*[text() =' " + spareName + "']"));
     }
 }
