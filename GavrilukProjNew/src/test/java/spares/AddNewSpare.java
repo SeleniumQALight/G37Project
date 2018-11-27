7111c963da476a03b92a7ad21f2d24e2f3df6219
OlenaG <olena.h.w@gmail.com>
2018-11-27 21:50:28 +0200

    27.11.2018

--- GavrilukProjNew/src/test/java/spares/AddNewSpare.java
+++ GavrilukProjNew/src/test/java/spares/AddNewSpare.java
@@ -1,9 +1,13 @@
 package spares;
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
+import pages.SparePage;
 import parenTest.ParentTest;
 
 public class AddNewSpare extends ParentTest{
+    String spareName = "gavryliukSpare";
     @Test
     public void addNewSpare(){
         loginPage.validLoginInToApp();
@@ -11,8 +15,18 @@ public class AddNewSpare extends ParentTest{
         homePage.clickOnMenuDictionary();
         homePage.clickOnSubMenuSpare();
         sparePage.checkUrl();
+     //   sparePage.checkAndDeleteSpare(spareName);
+        sparePage.deletingSpareUntilPresent(spareName);
+        sparePage.clickOnAddButton();
+        editSparePage.enterSpareNameInToImput(spareName);
+        editSparePage.selectSpareTypeInDD("Механика1");
+        editSparePage.clickButtonCreate();
 
+        checkExpectedResult("Spare was not added", sparePage.isSpareInList(spareName));
 
     }
-
+    @After
+    public void spareDeleting(){
+        sparePage.deletingSpareUntilPresent(spareName);
+    }
 }
