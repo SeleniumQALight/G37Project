b85d30fcb1752ada5d844dcce9dfec686e36e6cf
qalight <radulenko@gmail.com>
2018-11-29 21:47:10 +0200

    suit, params

--- overAll/src/main/java/libs/ActionsWithOurElements.java
+++ overAll/src/main/java/libs/ActionsWithOurElements.java
@@ -20,35 +20,35 @@ public class ActionsWithOurElements {
         wait10 = new WebDriverWait(webDriver, 10);
     }
 
-    public void enterTextInToElement(WebElement element, String text){
+    public void enterTextInToElement(WebElement element, String text) {
         try {
             element.clear();
             element.sendKeys(text);
             logger.info(text + " was inputted into Input");
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Cannot work with element" + e);
             Assert.fail("Cannot work with element" + e);
         }
     }
 
     public void clickOnElement(WebElement element) {
-        try{
+        try {
             wait5.until(ExpectedConditions.elementToBeClickable(element));
 //            wait5.until(ExpectedConditions.not(
 //                    ExpectedConditions.elementToBeClickable(element)));
 
             element.click();
             logger.info("Element was clicked ");
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Cannot work with element" + e);
             Assert.fail("Cannot work with element" + e);
         }
     }
 
-    public boolean isElementDisplayed(WebElement element){
+    public boolean isElementDisplayed(WebElement element) {
         try {
             return element.isDisplayed();
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
@@ -58,17 +58,41 @@ public class ActionsWithOurElements {
             Select select = new Select(element);
             select.selectByVisibleText(text);
             logger.info(text + " was selected in DD");
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Cannot work with element" + e);
             Assert.fail("Cannot work with element" + e);
         }
     }
 
     public boolean isElementDisplayed(By by) {
-        try{
+        try {
             return isElementDisplayed(webDriver.findElement(by));
-        }catch (Exception e) {
+        } catch (Exception e) {
             return false;
+        }
+    }
+
+    /**
+     * Set needed state
+     * @param element
+     * @param state (Only !!! check or uncheck)
+     */
+    public void setNeededStateToCheckBox(WebElement element, String state) {
+        boolean checkState = state.toLowerCase().equals("check");
+        boolean unCheckState = state.toLowerCase().equals("uncheck");
+        if (checkState || unCheckState) {
+            if (element.isSelected() && checkState) {
+                logger.info("Checkbox is already checked");
+            } else if (element.isSelected() && unCheckState) {
+                clickOnElement(element);
+            } else if (!element.isSelected() && checkState) {
+                clickOnElement(element);
+            } else if (!element.isSelected() && unCheckState) {
+                logger.info("Checkbox is already unchecked");
+            }
+        } else {
+            logger.error("State should be check or uncheck");
+            Assert.fail("State should be check or uncheck");
         }
     }
 
