9e61d1ca6b619288d533b4477498361afb20ea2a
bpysarenko <4spysar@gmail.com>
2018-12-01 16:50:54 +0200

    Homework - Update test AddProvider with parameters

--- PysarenkoProj/src/test/java/addProviders/AddProviders.java
+++ PysarenkoProj/src/test/java/addProviders/AddProviders.java
@@ -1,12 +1,34 @@
 package addProviders;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import parentTest.Parent_Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 
 public class AddProviders extends Parent_Test {
 
-    String providerFName = "Coala Fizlitso 100%";
-    String providerAddress = "Hong-Kong";
+    String providerFName;
+    String providerAddress;
+
+    public AddProviders(String providerFName, String providerAddress) {
+        this.providerFName = providerFName;
+        this.providerAddress = providerAddress;
+    }
+
+    @Parameterized.Parameters(name = "Parameters are{0} and {1}")
+    public static Collection testData(){
+        return Arrays.asList(new Object[][]{
+                {"Джигит1", "Мандариновая республика"},
+                {"WonderWoman,Inc.","Cozy street 14,NY,USA"},
+                {"1089s7ej", " ,owe 00"},
+
+        });
+    }
 
     @Test
     public void addProviderAsFizLitso(){
