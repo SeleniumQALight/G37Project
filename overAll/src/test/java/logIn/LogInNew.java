b0d99b123f14110c36fc8beeb04dff8bff1893ad
qalight <terraco_kiev@ukr.net>
2018-12-06 21:36:15 +0200

    DbBase

--- overAll/src/test/java/logIn/LogInNew.java
+++ overAll/src/test/java/logIn/LogInNew.java
@@ -1,15 +1,20 @@
 package logIn;
 
+import libs.UtilsForDB;
 import org.junit.Assert;
 import org.junit.Test;
 import parentTest.ParentTest;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class LogInNew extends ParentTest{
     @Test
-    public void validLogin(){
+    public void validLogin() throws SQLException, IOException, ClassNotFoundException {
+        UtilsForDB utilsForDB=new UtilsForDB();
         loginPage.openLoginPage();
         loginPage.enterLogin("Student");
-        loginPage.enterPassword("909090");
+        loginPage.enterPassword(utilsForDB.getPassForLogin("Student"));
         loginPage.clickButtonVhod();
 
         Assert.assertTrue("Avatar is not present", homePage.isAvatarDislpayed());
